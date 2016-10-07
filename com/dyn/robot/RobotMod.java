f9cc09e68e0593ee3bb901a0aea5dea19345a041
DomAmato <dominic.a.amato@gmail.com>
2016-10-07 12:11:07 -0500

    remove unused

--- com/dyn/robot/RobotMod.java
+++ com/dyn/robot/RobotMod.java
@@ -4,23 +4,18 @@ import com.dyn.robot.entity.BlockDynRobot;
 import com.dyn.robot.entity.DynRobotEntity;
 import com.dyn.robot.items.ItemDynRobotSpawner;
 import com.dyn.robot.items.ItemRemote;
-import com.dyn.robot.programs.UserProgramLibrary;
 import com.dyn.robot.proxy.Proxy;
 import com.dyn.robot.reference.MetaData;
 import com.dyn.robot.reference.Reference;
 
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLServerStartedEvent;
-import net.minecraftforge.fml.common.event.FMLServerStoppedEvent;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.relauncher.Side;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, dependencies = "required-after:dyn|server;after:ComputerCraft")
 public class RobotMod {
@@ -42,20 +37,6 @@ public class RobotMod {
 		proxy.init();
 		proxy.registerBlockItem(dynRobot);
 		proxy.registerItem(dynRobotRemote, dynRobotRemote.getUnlocalizedName(), 0);
-	}
-
-	@Mod.EventHandler
-	public void onServerStart(FMLServerStartedEvent event) {
-		if (FMLCommonHandler.instance().getEffectiveSide() == Side.SERVER) {
-			UserProgramLibrary.resetLibraryCache();
-		}
-	}
-
-	@Mod.EventHandler
-	public void onServerStopped(FMLServerStoppedEvent event) {
-		if (FMLCommonHandler.instance().getEffectiveSide() == Side.SERVER) {
-			UserProgramLibrary.resetLibraryCache();
-		}
 	}
 
 	@Mod.EventHandler
