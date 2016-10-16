520b99cec68f5bd67b0e70a32e62cd7172337b6b
Dominic Amato <dominic.a.amato@gmail.com>
2016-10-16 14:25:13 -0500

    dump old code and parse stuff

--- com/dyn/robot/code/parsetree/InvalidVariable.java
+++
@@ -1,9 +0,0 @@
-package com.dyn.robot.code.parsetree;
-
-import com.dyn.robot.code.Range;
-
-public class InvalidVariable extends Variable {
-	public InvalidVariable(Range range) {
-		super(range, "???");
-	}
-}
