6cea361926c430f8f04563c1aa88030c2da5b5d8
Dominic Amato <dominic.a.amato@gmail.com>
2016-12-05 15:02:34 -0600

    make spawnable ownerless robots (for admins)

--- items/ItemDynRobotSpawner.java
+++ items/ItemDynRobotSpawner.java
@@ -1,33 +1,37 @@
 package com.dyn.robot.items;
 
+import com.dyn.robot.entity.DynRobotEntity;
+
 import net.minecraft.block.Block;
-import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemMonsterPlacer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
 
-public class ItemDynRobotSpawner extends ItemBlock {
+public class ItemDynRobotSpawner extends Item{
 
-	public ItemDynRobotSpawner(Block block) {
-		super(block);
-		setCreativeTab(CreativeTabs.tabRedstone);
+	public ItemDynRobotSpawner() {
 	}
 
-	// @Override
-	// public boolean onItemUse(ItemStack is, EntityPlayer p, World worldIn,
-	// BlockPos pos, EnumFacing side, float hitX,
-	// float hitY, float hitZ) {
-	// if (worldIn.isRemote) {
-	// return super.onItemUse(is, p, worldIn, pos, side, hitX, hitY, hitZ);
-	// }
-	//
-	// DynRobotEntity robot = (DynRobotEntity)
-	// ItemMonsterPlacer.spawnCreature(worldIn,
-	// EntityList.classToStringMapping.get(DynRobotEntity.class), pos.getX() +
-	// 0.5, pos.getY() + 1.3, pos.getZ() + 0.5);
-	// if (robot != null) {
-	// robot.setTamer(p);
-	// return true;
-	// }
-	// return false;
-	// }
+	@Override
+	public boolean onItemUse(ItemStack is, EntityPlayer p, World worldIn, BlockPos pos, EnumFacing side, float hitX,
+			float hitY, float hitZ) {
+		if (worldIn.isRemote) {
+			return super.onItemUse(is, p, worldIn, pos, side, hitX, hitY, hitZ);
+		}
+
+		DynRobotEntity robot = (DynRobotEntity) ItemMonsterPlacer.spawnCreature(worldIn,
+				EntityList.classToStringMapping.get(DynRobotEntity.class), pos.getX() + 0.5, pos.getY() + 1.3,
+				pos.getZ() + 0.5);
+		if (robot != null) {
+			return true;
+		}
+		return false;
+	}
 
 }
