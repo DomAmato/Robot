4a8bf968c24635c26278df1c534562889f68b518
Dominic Amato <dominic.a.amato@gmail.com>
2017-03-03 14:40:03 -0600

    jump ai update

--- entity/ai/EntityAIJumpToward.java
+++ entity/ai/EntityAIJumpToward.java
@@ -2,11 +2,14 @@ package com.dyn.robot.entity.ai;
 
 import com.dyn.DYNServerMod;
 import com.dyn.robot.entity.EntityRobot;
-import com.dyn.utils.HelperFunctions;
+import com.dyn.server.network.NetworkManager;
+import com.dyn.server.network.packets.client.RobotPathMessage;
 
 import mobi.omegacentauri.raspberryjammod.RaspberryJamMod;
 import mobi.omegacentauri.raspberryjammod.network.CodeEvent;
 import net.minecraft.entity.ai.EntityAIBase;
+import net.minecraft.pathfinding.PathEntity;
+import net.minecraft.pathfinding.PathPoint;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.MathHelper;
 
@@ -38,10 +41,27 @@ public class EntityAIJumpToward extends EntityAIBase {
 				BlockPos prevLoc = leaper.getProgramPath().iterator().next();
 				leaper.getProgramPath().remove(prevLoc);
 			}
-			leaper.setPosition(leaper.getPosition().getX() + .5, leaper.getPosition().getY(),
-					leaper.getPosition().getZ() + .5);
-			leaper.rotate(HelperFunctions.getAngleFromFacing(leaper.getProgrammedDirection()));
+			leaper.getNavigator().tryMoveToXYZ((leaper.getPosition().getX()) + 0.5D, leaper.getPosition().getY(),
+					(leaper.getPosition().getZ()) + 0.5D, 0.4F);
+			// leaper.setPosition(leaper.getPosition().getX() + .5,
+			// leaper.getPosition().getY(),
+			// leaper.getPosition().getZ() + .5);
+			// leaper.rotate(HelperFunctions.getAngleFromFacing(leaper.getProgrammedDirection()));
 			leaper.InsertToProgramPath(0, leaper.getPosition());
+
+			if (DYNServerMod.developmentEnvironment) {
+				NetworkManager
+						.sendToDimension(
+								new RobotPathMessage(
+										new PathEntity(new PathPoint[] {
+												new PathPoint(leaper.getPosition().getX(), leaper.getPosition().getY(),
+														leaper.getPosition().getZ()),
+												new PathPoint(leaper.getPosition().getX(), leaper.getPosition().getY(),
+														leaper.getPosition().getZ()) }),
+										leaper.getEntityId()),
+								leaper.dimension);
+			}
+			leaper.resumeExecution();
 		}
 		return doContinue;
 	}
@@ -53,6 +73,7 @@ public class EntityAIJumpToward extends EntityAIBase {
 	public boolean shouldExecute() {
 		if (leaper.getShouldJump()) {
 			leaper.setShouldJump(false);
+			leaper.pauseCodeExecution();
 			return true;
 		}
 		return false;
@@ -63,7 +84,7 @@ public class EntityAIJumpToward extends EntityAIBase {
 	 */
 	@Override
 	public void startExecuting() {
-		leapTarget = leaper.getPosition().offset(leaper.getProgrammedDirection(), 2);
+		leapTarget = leaper.getPosition().offset(leaper.getProgrammedDirection(), 1);
 		double d0 = (leapTarget.getX() + .5) - leaper.posX;
 		double d1 = (leapTarget.getZ() + .5) - leaper.posZ;
 		float f = MathHelper.sqrt_double((d0 * d0) + (d1 * d1));
@@ -71,5 +92,15 @@ public class EntityAIJumpToward extends EntityAIBase {
 		leaper.motionZ += ((d1 / f) * 0.5D * 0.800000011920929D) + (leaper.motionZ * 0.20000000298023224D);
 		leaper.motionY = leapMotionY;
 		leaper.InsertToProgramPath(0, leapTarget);
+		if (DYNServerMod.developmentEnvironment) {
+			NetworkManager
+					.sendToDimension(
+							new RobotPathMessage(new PathEntity(new PathPoint[] {
+									new PathPoint(leaper.getPosition().getX(), leaper.getPosition().getY(),
+											leaper.getPosition().getZ()),
+									new PathPoint(leapTarget.getX(), leapTarget.getY(), leapTarget.getZ()) }),
+									leaper.getEntityId()),
+							leaper.dimension);
+		}
 	}
 }
