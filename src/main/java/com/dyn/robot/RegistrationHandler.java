80a048ed96e382eca7bce9fd9290e85bae721e9e
Dominic Amato <dominic.a.amato@gmail.com>
2018-05-08 23:58:18 -0500

    start work on auto generating api files

--- src/main/java/com/dyn/robot/RegistrationHandler.java
+++ src/main/java/com/dyn/robot/RegistrationHandler.java
@@ -1,13 +1,23 @@
 package com.dyn.robot;
 
+import java.util.Map.Entry;
+
 import com.dyn.robot.entity.SimpleRobotEntity;
 import com.dyn.robot.reference.Reference;
 
 import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.init.Biomes;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemEnchantedBook;
+import net.minecraft.item.ItemMonsterPlacer;
+import net.minecraft.item.ItemPotion;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemTippedArrow;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraftforge.event.RegistryEvent;
@@ -31,6 +41,28 @@ public class RegistrationHandler {
 	public static void registerBlocks(final RegistryEvent.Register<Block> event) {
 		event.getRegistry().register(RobotMod.robot_block);
 		event.getRegistry().register(RobotMod.robot_magent);
+		StringBuilder sb = new StringBuilder();
+		sb.append("from .block import Block\n\n");
+		for (Entry<ResourceLocation, Block> entry : event.getRegistry().getEntries()) {
+			Item bItem = Item.getItemFromBlock(entry.getValue());
+			if (!(bItem instanceof ItemPotion) && !(bItem instanceof ItemEnchantedBook)
+					&& !(bItem instanceof ItemMonsterPlacer) && !(bItem instanceof ItemTippedArrow)) {
+				NonNullList<ItemStack> items = NonNullList.create();
+				bItem.getSubItems(CreativeTabs.SEARCH, items);
+				for (ItemStack item : items) {
+					if (Block.getBlockFromItem(item.getItem()) != Blocks.AIR) {
+						Block block = Block.getBlockFromItem(item.getItem());
+						sb.append(String.format("%1$-33s",
+								item.getDisplayName().toUpperCase().replaceAll(" ", "_").replaceAll("([#'\\)\\(])+",
+										""))
+								+ " = Block(" + Block.getIdFromBlock(block) + ", " + item.getMetadata() + ", \""
+								+ item.getDisplayName() + "\")\n");
+					}
+				}
+			}
+
+		}
+		sb.toString();
 	}
 
 	@SubscribeEvent
@@ -63,6 +95,29 @@ public class RegistrationHandler {
 		event.getRegistry().register(RobotMod.whistle);
 		event.getRegistry().register(RobotMod.neuralyzer);
 		event.getRegistry().register(RobotMod.manual);
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("from .item import Item\n\n");
+		for (Entry<ResourceLocation, Item> entry : event.getRegistry().getEntries()) {
+			if (!(entry.getValue() instanceof ItemPotion) && !(entry.getValue() instanceof ItemEnchantedBook)
+					&& !(entry.getValue() instanceof ItemMonsterPlacer)
+					&& !(entry.getValue() instanceof ItemTippedArrow)) {
+				NonNullList<ItemStack> items = NonNullList.create();
+				entry.getValue().getSubItems(CreativeTabs.SEARCH, items);
+				for (ItemStack item : items) {
+					if (Block.getBlockFromItem(item.getItem()) != Blocks.AIR) {
+						Block block = Block.getBlockFromItem(item.getItem());
+						sb.append(String.format("%1$-33s",
+								item.getDisplayName().toUpperCase().replaceAll(" ", "_").replaceAll("([#'\\)\\(])+",
+										""))
+								+ " = Item(" + Block.getIdFromBlock(block) + ", " + item.getMetadata() + ", \""
+								+ item.getDisplayName() + "\")\n");
+					}
+				}
+			}
+
+		}
+		sb.toString();
 	}
 
 	/**
