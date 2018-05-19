671cc9928136acd95528d53812cdbfa5184df67a
Dominic Amato <dominic.a.amato@gmail.com>
2018-05-19 15:40:25 -0500

    fix robot spawner

--- src/main/java/com/dyn/robot/items/ItemSimpleRobotSpawner.java
+++ src/main/java/com/dyn/robot/items/ItemSimpleRobotSpawner.java
@@ -5,6 +5,9 @@ import com.dyn.robot.entity.SimpleRobotEntity;
 import com.dyn.robot.reference.Reference;
 
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.IEntityLivingData;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemMonsterPlacer;
@@ -15,6 +18,7 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -55,12 +59,17 @@ public class ItemSimpleRobotSpawner extends Item {
 		if (worldIn.isRemote) {
 			return super.onItemUse(player, worldIn, pos, hand, facing, hitX, hitY, hitZ);
 		}
-		SimpleRobotEntity robot = (SimpleRobotEntity) ItemMonsterPlacer.spawnCreature(worldIn,
-				new ResourceLocation(Reference.MOD_ID, "RobotEntity"), pos.getX() + 0.5, pos.getY() + 1.3,
-				pos.getZ() + 0.5);
+		SimpleRobotEntity robot = (SimpleRobotEntity) EntityList.createEntityByIDFromName(new ResourceLocation(Reference.MOD_ID, "robot"), worldIn);
 		if (robot == null) {
 			return EnumActionResult.FAIL;
 		}
+		robot.setLocationAndAngles(pos.getX() + 0.5, pos.getY() + 1.3,
+ 				pos.getZ() + 0.5, MathHelper.wrapDegrees(worldIn.rand.nextFloat() * 360.0F), 0.0F);
+         robot.rotationYawHead = robot.rotationYaw;
+         robot.renderYawOffset = robot.rotationYaw;
+         robot.onInitialSpawn(worldIn.getDifficultyForLocation(new BlockPos(robot)), (IEntityLivingData)null);
+         worldIn.spawnEntity(robot);
+         robot.playLivingSound();
 		if (player.getHeldItemMainhand().getMetadata() == 0) {
 			robot.setTamable(true);
 		} else {
